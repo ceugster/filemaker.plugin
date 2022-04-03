d806f422820d387e66092c58137bec3c42742446
Christian Eugster <christian.eugster@gmx.net>
2022-04-03 14:47:59 +0200

    Changed executor command from CreateQRCode to CreateQRBill

--- ch.eugster.filemaker.fsl.plugin/src/ch/eugster/filemaker/fsl/plugin/camt05400102/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2022.03.19 um 03:09:33 PM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:camt.054.001.02", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ch.eugster.filemaker.fsl.plugin.camt05400102;
--- ch.eugster.filemaker.fsl.plugin/src/ch/eugster/filemaker/fsl/plugin/camt05400108/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2022.03.20 um 07:56:34 AM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:camt.054.001.08", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ch.eugster.filemaker.fsl.plugin.camt05400108;
